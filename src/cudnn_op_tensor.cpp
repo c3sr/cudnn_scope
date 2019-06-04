03b2d995046618fc4d03be12da3622162a5fecf9
Abdul Dakkak <abduld@wolfram.com>
2019-06-04 08:32:41 +0100

    updates generated

--- src/cudnn_op_tensor.cpp
+++ src/cudnn_op_tensor.cpp
@@ -166,10 +166,11 @@ static void LAYER_CUDNN_OP_TENSOR_BWD_Impl(benchmark::State& state) {
   LAYER_CUDNN_OP_TENSOR_FWD_Impl<T, op_type>(state);
 }
 
-#define _DECLARE_OP(DIRECTION, OP_NAME, OP_TYPE) template <typename T> \
-static void LAYER_CUDNN_OP_TENSOR_##OP_NAME##_##DIRECTION##_Impl(benchmark::State& state) { \
-  LAYER_CUDNN_OP_TENSOR_BWD_Impl<T, OP_TYPE>(state); \
-}
+#define _DECLARE_OP(DIRECTION, OP_NAME, OP_TYPE)                                                                       \
+  template <typename T>                                                                                                \
+  static void LAYER_CUDNN_OP_TENSOR_##OP_NAME##_##DIRECTION##_Impl(benchmark::State& state) {                          \
+    LAYER_CUDNN_OP_TENSOR_BWD_Impl<T, OP_TYPE>(state);                                                                 \
+  }
 
 #define DECLARE_OP(OP_NAME, OP_TYPE)                                                                                   \
   _DECLARE_OP(FWD, OP_NAME, OP_TYPE)                                                                                   \
