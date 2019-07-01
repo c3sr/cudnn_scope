cd61e2427f1596d8c03f02e48aebae55195f782a
Abdul Dakkak <dakkak@illinois.edu>
2019-07-01 01:10:48 +0000

    enable all

--- src/sugar.cmake
+++ src/sugar.cmake
@@ -19,26 +19,26 @@ sugar_files(cudnn_BENCHMARK_HEADERS
             utils.hpp)
 
 sugar_files(cudnn_BENCHMARK_SOURCES
-            # ctc_loss.cpp
-            # cublas_gemm_bwd.cpp
-            # cublas_gemm_fwd.cpp
-            # cublas_gemv_bwd.cpp
-            # cublas_gemv_fwd.cpp
-            # cudnn_activation_bwd.cpp
-            # cudnn_activation_fwd.cpp
-            # cudnn_add_tensor.cpp
-            # cudnn_batchnorm_bwd.cpp
-            # cudnn_batchnorm_fwd.cpp
-            # cudnn_conv_bias_activation_fwd.cpp
-            # cudnn_conv_bwd_bias.cpp
-            # cudnn_conv_bwd_data.cpp
-            # cudnn_conv_bwd_filter.cpp
+            ctc_loss.cpp
+            cublas_gemm_bwd.cpp
+            cublas_gemm_fwd.cpp
+            cublas_gemv_bwd.cpp
+            cublas_gemv_fwd.cpp
+            cudnn_activation_bwd.cpp
+            cudnn_activation_fwd.cpp
+            cudnn_add_tensor.cpp
+            cudnn_batchnorm_bwd.cpp
+            cudnn_batchnorm_fwd.cpp
+            cudnn_conv_bias_activation_fwd.cpp
+            cudnn_conv_bwd_bias.cpp
+            cudnn_conv_bwd_data.cpp
+            cudnn_conv_bwd_filter.cpp
             cudnn_conv_fwd.cpp
-            # cudnn_dropout_bwd.cpp
-            # cudnn_dropout_fwd.cpp
-            # cudnn_op_tensor.cpp
-            # cudnn_pooling_bwd.cpp
-            # cudnn_pooling_fwd.cpp
+            cudnn_dropout_bwd.cpp
+            cudnn_dropout_fwd.cpp
+            cudnn_op_tensor.cpp
+            cudnn_pooling_bwd.cpp
+            cudnn_pooling_fwd.cpp
             cudnn_softmax_bwd.cpp
             cudnn_softmax_fwd.cpp
             init.cpp)
