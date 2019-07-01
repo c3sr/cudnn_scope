f7ff6766cb488a5c78f6324316372db7e141c57f
Abdul Dakkak <dakkak@illinois.edu>
2019-07-01 07:34:49 +0000

    update ops

--- scripts/run_benchmarks_profile.sh
+++ scripts/run_benchmarks_profile.sh
@@ -9,7 +9,7 @@ pushd ${SCOPE_TOP_DIR}
 
 
 # CUPTI_METRICS="flop_count_sp,flop_count_dp,flop_count_sp_add,dram_read_bytes,dram_write_bytes,flop_count_sp_add,flop_count_sp_fma,flop_count_sp_mul,flop_count_sp_special,inst_executed,inst_issued,inst_fp_32,inst_fp_64,inst_integer,inst_bit_convert,inst_control,ipc"
-CUPTI_METRICS="flop_count_sp,flop_count_dp,dram_read_bytes,dram_write_bytes"
+# CUPTI_METRICS="flop_count_sp,flop_count_dp,dram_read_bytes,dram_write_bytes"
 CMAKE_OPTIONS="-DENABLE_CUDNN=ON -DENABLE_CUDNN_DLPERF=ON -DENABLE_COMM=OFF -DENABLE_EXAMPLE=OFF -DCMAKE_BUILD_TYPE=Release -DENABLE_CUDNN_CUPTI=ON -DCUDNN_CUPTI_NUM_ITERS=10"
 
 rm -fr ${RESULTS_DIR}
@@ -23,7 +23,7 @@ do
   pushd build
   cmake .. ${CMAKE_OPTIONS} -DCUDNN_BATCH_SIZE=${BATCH_SIZE}
   make -j $(nproc)
-  ./scope --benchmark_out_format=json --benchmark_out=${RESULTS_DIR}/${BATCH_SIZE}.json --metrics=${CUPTI_METRICS}
+  ./scope --benchmark_out_format=json --benchmark_out=${RESULTS_DIR}/${BATCH_SIZE}.json #--metrics=${CUPTI_METRICS}
   popd
 done
 
