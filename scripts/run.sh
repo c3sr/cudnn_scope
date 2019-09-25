eed9662e9cc03e14bd2eb4aa0efba3c86c4f2789
Abdul Dakkak <abduld@wolfram.com>
2019-09-25 08:09:08 -0500

    updatte

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 sudo ${DIR}/../setup.sh
 
-rm -fr results 
+rm -fr ${DIR}/../results 
 
 ${DIR}/run_benchmarks.sh
 ${DIR}/run_resnet50_benchmarks.sh
@@ -13,4 +13,4 @@ ${DIR}/run_mobilenetv2_benchmarks.sh
 ${DIR}/run_benchmark_profile.sh
 
 unalias cp
-cp -r results $GOPATH/src/github.com/rai-project/dlperf
+cp -r ${DIR}/../results $GOPATH/src/github.com/rai-project/dlperf
