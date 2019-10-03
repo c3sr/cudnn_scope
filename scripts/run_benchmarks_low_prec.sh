0e18a6fe42db526161ab85c21b69c4050063c0c9
Abdul Dakkak <dakkak@illinois.edu>
2019-10-03 14:38:40 +0000

    update

--- scripts/run_benchmarks_low_prec.sh
+++ scripts/run_benchmarks_low_prec.sh
@@ -17,11 +17,11 @@ nvidia-smi -x -q -a > ${RESULTS_DIR}/nvidia_smi.xml
 
 declare -a batch_sizes=(
   1 \
-  # 2 \
-  # 4 \
-  # 8 \
-  # 16 \
-  # 32 \
+  2 \
+  4 \
+  8 \
+  16 \
+  32 \
   # 64 \
   # 128 \
   # 256 \
