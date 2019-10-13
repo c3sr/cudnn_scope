6915d91f3e5298dea5e3bedaab69b6d3092422da
Abdul Dakkak <abduld@wolfram.com>
2019-10-13 05:58:34 -0500

    update

---
+++ scripts/run_all_low_prec.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+sudo bash -c ${DIR}/../setup/setup.sh
+
+rm -fr ${DIR}/../results
+
+${DIR}/run_benchmarks_low_prec_nhwc_layout.sh
+${DIR}/run_benchmarks_low_prec_padded_nhwc_layout.sh
+${DIR}/run_benchmarks_low_prec_padded.sh
+${DIR}/run_benchmarks_low_prec.sh
+# ${DIR}/run_benchmarks_low_prec_profile.sh
+
+
+cp -r ${DIR}/../results $GOPATH/src/github.com/rai-project/dlperf
